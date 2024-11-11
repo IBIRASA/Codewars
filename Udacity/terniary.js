4bb61fed183a6a08308416f9484139dc47f6700d
IBIRASA <d.ishimwe@alustudent.com>
2024-11-11 17:01:24 +0200

    updated

---
+++ Udacity/terniary.js
@@ -0,0 +1,12 @@
+// var color = "green";
+// if(color)
+// {
+//     console.log("The color is green");
+// }
+// else{
+//     console.log("The color is not green");
+// }
+
+var color ="green";
+var color = "green" ? "The color is green" : "The color is not green";
+console.log(color);
