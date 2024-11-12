d568a45e792aab67500a503b0ff069443727f39b
IBIRASA <d.ishimwe@alustudent.com>
2024-11-12 17:27:51 +0200

    without_loopd

---
+++ Udacity/whileloops.js
@@ -0,0 +1,17 @@
+/* Used to repeat a blokc of code as long as a certian condition is true)*/
+/*sytnax of while loops*/
+/* while(condition){}*/
+
+
+// let a =1;
+// while(a <=5)
+// {
+//     console.log(a);
+//     a++;
+// }
+
+console.log(1);
+console.log(2);
+console.log(3);
+console.log(4);
+console.log(5);
