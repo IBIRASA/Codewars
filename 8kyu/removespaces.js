2d0c356470caba22054381ba39f0621a01518028
IBIRASA <d.ishimwe@alustudent.com>
2024-11-14 17:51:37 +0200

    updated

--- 8kyu/removespaces.js
+++ 8kyu/removespaces.js
@@ -1,7 +1,13 @@
-function removeSpaces(str) {
-  return str.replace(/\s+/g, '');
-}
+// function removeSpaces(str) {
+//   return str.replace(/\s+/g, '');
+// }
 
-console.log(removeSpaces("8 j 8   mBliB8g  imjB8B8  jl  B"));
-console.log(removeSpaces("8 8 Bi fk8h B 8 BB8B B B  B888 c hl8 BhB fd"));
-console.log(removeSpaces("8aaaaa dddd r     "));
+// console.log(removeSpaces("8 j 8   mBliB8g  imjB8B8  jl  B"));
+// console.log(removeSpaces("8 8 Bi fk8h B 8 BB8B B B  B888 c hl8 BhB fd"));
+// console.log(removeSpaces("8aaaaa dddd r     "));
+
+
+let str = "Hello, world!";
+let newStr = str.replace("world", "everyone");
+console.log(newStr);
+
