66c26b9b0f9e173fd0a47c5d0a7863852d228e53
IBIRASA <d.ishimwe@alustudent.com>
2024-11-14 17:32:30 +0200

    arrays

---
+++ Udacity/Arrays.js
@@ -0,0 +1,21 @@
+// var donuts = ["glazed", "chocolate frosted", "Boston creme", "glazed cruller", "cinnamon sugar", "sprinkled", "powdered"];
+
+// donuts.pop();
+// donuts.pop(); 
+// donuts.pop();
+
+// console.log(donuts);
+
+
+
+
+
+// var donuts = ["glazed", "chocolate frosted", "Boston creme", "glazed cruller", "cinnamon sugar", "sprinkled"];
+// donuts.push("powdered"); 
+// console.log(donuts)
+
+
+
+
+var donuts = ["glazed", "chocolate frosted", "Boston creme", "glazed cruller", "cinnamon sugar", "sprinkled"];
+console.log(donuts.length)
