d558ceaeed8d110fc404623832c7d5599a6d75cd
IBIRASA <d.ishimwe@alustudent.com>
2024-11-14 17:46:13 +0200

    updated

--- 8kyu/removespaces.js
+++ 8kyu/removespaces.js
@@ -2,7 +2,6 @@ function removeSpaces(str) {
   return str.replace(/\s+/g, '');
 }
 
-// Examples
 console.log(removeSpaces("8 j 8   mBliB8g  imjB8B8  jl  B"));
 console.log(removeSpaces("8 8 Bi fk8h B 8 BB8B B B  B888 c hl8 BhB fd"));
 console.log(removeSpaces("8aaaaa dddd r     "));
